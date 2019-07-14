2fb88745a412963c4619ae2184ae80efe35532b5
Alaric von Teplitz <alaric2000@gmail.com>
2019-07-14 15:08:46 +0300

    Updates

---
+++ precache-manifest.1302ec9c90c3c903fed28d6d5bd5e564.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2011b47d7789ac7a24833073767c0967",
+    "url": "/dragonsweeper/index.html"
+  },
+  {
+    "revision": "859e3cfede1760c9e8b7",
+    "url": "/dragonsweeper/static/css/main.b20f21b0.chunk.css"
+  },
+  {
+    "revision": "71257fcd22cb79dd3fb1",
+    "url": "/dragonsweeper/static/js/2.19373ec0.chunk.js"
+  },
+  {
+    "revision": "859e3cfede1760c9e8b7",
+    "url": "/dragonsweeper/static/js/main.80ee943b.chunk.js"
+  },
+  {
+    "revision": "f96bc0b18a39252b8c0d",
+    "url": "/dragonsweeper/static/js/runtime~main.7ab4671e.js"
+  },
+  {
+    "revision": "93231d199429c53cce08ff3b89c5dadf",
+    "url": "/dragonsweeper/static/media/clear.93231d19.mp3"
+  },
+  {
+    "revision": "4a8e83ad296da1538881824101ab01f6",
+    "url": "/dragonsweeper/static/media/defeat.4a8e83ad.mp3"
+  },
+  {
+    "revision": "61f86c970ef2d4b0cafde356198f10ff",
+    "url": "/dragonsweeper/static/media/dragon0.61f86c97.png"
+  },
+  {
+    "revision": "2447a1731914ca2d7bae3f81dfd6f947",
+    "url": "/dragonsweeper/static/media/dragon2.2447a173.png"
+  },
+  {
+    "revision": "27b9efe7ae34478c82311efb90011607",
+    "url": "/dragonsweeper/static/media/librebaskerville.27b9efe7.ttf"
+  },
+  {
+    "revision": "ab98e5d7e95a9aa3e0b4f54f62d9e268",
+    "url": "/dragonsweeper/static/media/mark.ab98e5d7.mp3"
+  },
+  {
+    "revision": "8c1fe6f0b5380688e79034a633f54a79",
+    "url": "/dragonsweeper/static/media/open1.8c1fe6f0.png"
+  },
+  {
+    "revision": "e439d542266c4c873568a8c20be0c19e",
+    "url": "/dragonsweeper/static/media/open2.e439d542.png"
+  },
+  {
+    "revision": "9155b43c05b1c320a7a7227cca0cdfef",
+    "url": "/dragonsweeper/static/media/open3.9155b43c.png"
+  },
+  {
+    "revision": "f3df42abeb26a8c808eae4e288f6869b",
+    "url": "/dragonsweeper/static/media/start.f3df42ab.mp3"
+  },
+  {
+    "revision": "2fa69cd52dff8afd91ff484233d857d1",
+    "url": "/dragonsweeper/static/media/tutorial-closed.2fa69cd5.mp3"
+  },
+  {
+    "revision": "bbc18528fea4d55e12406a7ef463cfc2",
+    "url": "/dragonsweeper/static/media/tutorial-open.bbc18528.mp3"
+  },
+  {
+    "revision": "81f200de3e80c67a09971a18386bcea1",
+    "url": "/dragonsweeper/static/media/unmark.81f200de.mp3"
+  },
+  {
+    "revision": "a601b804cf583b7a7b63849db85c99b8",
+    "url": "/dragonsweeper/static/media/victory.a601b804.mp3"
+  },
+  {
+    "revision": "241dafeb751abb21665a860734cb0dbd",
+    "url": "/dragonsweeper/static/media/warning.241dafeb.mp3"
+  }
+]);
